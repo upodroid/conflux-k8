7646842c7a5eed2ea51ed5161014df98f357ff47
Upo <hi@upo.one>
2019-02-06 11:16:10 +0000

    init script changes

--- init.sh
+++ init.sh
@@ -2,19 +2,19 @@
 # More Args  https://github.com/helm/charts/tree/master/stable/nginx-ingress
 echo "Run kubectl create secret generic mysql --from-literal=somepw first before running this script. "
 echo "Also run kubectl create secret tls upodroid --key key.pem --cert cert.pem"
+sleep 5
 while true; do
     read -p "Have you set the env variable RESERVED_IP, authenticated kubectl with acc that has owner or container admin role and set the secret for mysql? " yn
     case $yn in
-        [Yy]* ) make install; break;;
+        [Yy]* ) kubectl create serviceaccount --namespace kube-system tiller
+        kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+        helm init --service-account tiller
+        sleep 10
+        helm install --name nginx-ingress stable/nginx-ingress --set controller.service.loadBalancerIP="$RESERVED_IP"
+        kubectl apply -f .; break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
 done
 
-kubectl create serviceaccount --namespace kube-system tiller
-kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-helm init --service-account tiller
-sleep 10
-helm install --name nginx-ingress stable/nginx-ingress --set controller.service.loadBalancerIP="$RESERVED_IP"
-kubectl apply -f .
 
