c3899aa5623f6ac8402c8cd3d2616690b1cb4263
Upo <hi@upo.one>
2019-02-08 17:39:28 +0000

    Friday Fix

--- ssl/ssl.sh
+++ ssl/ssl.sh
@@ -12,4 +12,23 @@ helm install \
   --namespace cert-manager \
   stable/cert-manager
 
+sleep 20
+
+aws_secret=`cat ~/.aws/secret.txt`
+kubectl --namespace cert-manager create secret generic route53-credentials --from-literal="secret-access-key=$aws_secret"
+
+
 kubectl apply -f issuer.yaml
+kubectl apply -f cert.yaml
+
+## Replicate cert to all namesspaces using kubed
+helm repo add appscode https://charts.appscode.com/stable/
+helm repo update
+helm install appscode/kubed --name kubed --namespace kube-system \
+    --set apiserver.ca="$(onessl get kube-ca)" \
+    --set config.clusterName=maker 
+
+sleep 10
+
+kubectl annotate secret upodroid-com-tls -n cert-manager kubed.appscode.com/sync="app=kubed"
+kubectl label namespace default app=kubed
