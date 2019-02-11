827cc5a5ed1200a9a710b09a279a3336ac417edb
Upo <hi@upo.one>
2019-02-11 09:56:11 +0000

    Improvements, reordered init scripts

---
+++ cluster.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source vars.sh
+gcloud container clusters create $CLUSTER_NAME \
+ --num-nodes=3 \
+ --disk-size=50 \
+ --machine-type g1-small \
+ --no-enable-legacy-authorization \
+ --image-type ubuntu \
+ --zone europe-west2-b 
--- init.sh
+++ init.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
-
+source vars.sh
 # Install helm + login with project owner or container-admin SA
 # More Args  https://github.com/helm/charts/tree/master/stable/nginx-ingress
 echo "Run kubectl create secret generic mysql --from-literal=password=somepw first before running this script. "
 echo "Also run kubectl create secret tls upodroid --key key.pem --cert cert.pem"
 
+gcloud container clusters get-credentials $CLUSTER_NAME
+
 kubectl create clusterrolebinding cluster-admin-binding \
   --clusterrole=cluster-admin \
   --user=$(gcloud config get-value core/account)
   
-source vars.sh
-gcloud container clusters get-credentials $CLUSTER_NAME
 sleep 5
 while true; do
     read -p "Have you set the env variable RESERVED_IP, authenticated kubectl with acc that has owner or container admin role and set the secret for mysql? " yn
     case $yn in
         [Yy]* ) kubectl create serviceaccount --namespace kube-system tiller
         kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-        helm init --service-account tiller
-        sleep 10
-        helm install --name nginx-ingress stable/nginx-ingress --set controller.service.loadBalancerIP=$RESERVED_IP
-        kubectl apply -f .; break;;
+        helm init --service-account tiller --wait
+        helm install --name nginx-ingress stable/nginx-ingress --set controller.service.loadBalancerIP=$RESERVED_IP; break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
 done
+
+PASSWORD=$(head -c 12 /dev/urandom | shasum| cut -d' ' -f1)
+echo $PASSWORD > mysql-$(date +%d-%m-%y).txt
+kubectl create secret generic mysql --from-literal=password=$PASSWORD
+bash ./ssl/ssl.sh
 
 
