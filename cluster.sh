0cd414b3fc87c8b70c6d457d8976e856b4caa734
Upo <hi@upo.one>
2019-02-13 13:19:30 +0000

    More fixes

--- cluster.sh
+++ cluster.sh
@@ -3,6 +3,7 @@ source vars.sh
 gcloud container clusters create $CLUSTER_NAME \
  --num-nodes=3 \
  --disk-size=50 \
+ --enable-ip-alias \
  --machine-type g1-small \
  --no-enable-legacy-authorization \
  --image-type ubuntu \
