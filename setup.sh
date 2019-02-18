480e810f13f9e73d19e30ba1e150c8034b8ee120
Upo <hi@upo.one>
2019-02-18 13:34:26 +0000

    YAML typos

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install -y python3 python3-pip httpie 
+sudo apt install -y python3 python3-pip httpie yamllint
 
 sudo snap install helm kubectl--classic
 
